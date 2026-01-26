da1e79b749ceac1faa48b37887729c18825d0a74
Anupam Yadav <anupamyadav4347@gmail.com>
2026-01-26 15:15:57 +0530

    .

--- src/DSA/06_frequencyInDictionary.kt
+++ src/DSA/06_frequencyInDictionary.kt
@@ -2,7 +2,7 @@ package DSA
 
 fun main() {
     val nums=listOf(1,2,3,4,3,2,1,4,5,6,7,8,9,9,0,1,2,2,23,3,9)
-    var frequency_map= mutableMapOf<Int, Int>()
+    val frequency_map= mutableMapOf<Int, Int>()
     for (i in nums){
         if (i !in frequency_map.keys ){
             frequency_map[i]=1

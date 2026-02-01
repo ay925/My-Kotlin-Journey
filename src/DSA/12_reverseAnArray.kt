d66cf36ef4fe3f64e9ee3b3d6256edf1450508c4
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-01 14:19:09 +0530

    .

---
+++ src/DSA/12_reverseAnArray.kt
@@ -0,0 +1,15 @@
+package DSA
+
+fun reverseAnArray(arr: IntArray,left:Int,right:Int): IntArray{
+    if (left>=right){
+        return arr
+    }
+    val temp=arr[right]
+    arr[right]=arr[left]
+    arr[left]=temp
+    return reverseAnArray(arr,left+1,right-1)
+}
+
+fun main() {
+    println(reverseAnArray(intArrayOf(1,2,3,4,5,6,7,8,9,0),0,9).joinToString())
+}

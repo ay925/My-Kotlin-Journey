b90f9938ed8fca918d6daa6b011c7a6c2c2a8c54
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-05 15:38:13 +0530

    .

---
+++ src/DSA/Leetcode/09_longestCommonPrefix.kt
@@ -0,0 +1,18 @@
+package DSA.Leetcode
+
+//class Solution {
+//    fun longestCommonPrefix(strs: Array<String>): String {
+//        val string=""
+//        for (i in strs){
+//            for(j in i){
+//                if (i[j]=="fl"){
+//
+//                }
+//            }
+//        }
+//        return string
+//    }
+//}
+//fun main() {
+//
+//}

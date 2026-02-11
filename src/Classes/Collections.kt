883ca6804b68020e5f8afe489bc13cfef2847d78
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-11 14:38:01 +0530

    .

--- src/Classes/Collections.kt
+++ src/Classes/Collections.kt
@@ -8,8 +8,23 @@ fun main() {
     println(list[0])
     list[0]=5
     println(list)
-    val list1=listOf(1,2,3,4,5,10)
+    val list1=listOf(0,2,3,4,5,10)
     list.addAll(list1)
-    println(list1)
+    println(list)
 
+
+    val subjectSet=setOf("CS","Physics","Chemistry","Maths","Physics","Chemistry")
+    println(subjectSet)
+
+    val map =mapOf(1 to "Raj",2 to "Anupam")
+    println(map)
+
+    val mutableMap=mutableMapOf<Int, String>()
+    mutableMap[1] = "Anupam"
+    mutableMap[2]="Aditi"
+    mutableMap[3]="Anu"
+    println(mutableMap)
+    for ((key,value) in mutableMap){
+        println("$key : $value")
+    }
 }

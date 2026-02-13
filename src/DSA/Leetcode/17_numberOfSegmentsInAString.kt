3e8c1ddc2f95101556a55320e28333fe8ba9bc0b
Anupam Yadav <anupamyadav4347@gmail.com>
2026-02-13 17:50:02 +0530

    .

--- src/DSA/Leetcode/17_numberOfSegmentsInAString.kt
+++ src/DSA/Leetcode/17_numberOfSegmentsInAString.kt
@@ -1,3 +1,21 @@
 package DSA.Leetcode
 
 
+class Solution17 {
+    fun countSegments(s: String): Int {
+        val str = s.trim()
+        if (str.isEmpty()) return 0
+
+        var count = 1
+        for (i in 1 until str.length) {
+            if (str[i] != ' ' && str[i - 1] == ' ') {
+                count++
+            }
+        }
+        return count
+    }
+}
+fun main(){
+    val q=Solution17()
+    print(q.countSegments(", , , ,        a, eaefa"))
+}

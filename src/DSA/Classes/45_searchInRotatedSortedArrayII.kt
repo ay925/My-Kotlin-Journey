4ba16bd9241d209191eff332e93b25a9bf13a60b
Anupam Yadav <anupamyadav4347@gmail.com>
2026-03-04 22:11:19 +0530

    .

---
+++ src/DSA/Classes/45_searchInRotatedSortedArrayII.kt
@@ -0,0 +1,46 @@
+package DSA.Classes
+
+fun main() {
+
+    val nums = intArrayOf(
+        1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1
+    )
+
+    var low = 0
+    var high = nums.size - 1
+    val target = 2
+    var ans = false
+
+    while (low <= high) {
+
+        val mid = (low + high) / 2
+
+        if (nums[mid] == target) {
+            ans = true
+            break
+        }
+
+        if (nums[mid] == nums[high]) {
+            high--
+        }
+        else if (nums[mid] <= nums[high]) {
+
+            if (target >= nums[mid] && target <= nums[high]) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+
+        } else {
+
+            if (target >= nums[low] && target <= nums[mid]) {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+
+        }
+    }
+
+    println(ans)
+}

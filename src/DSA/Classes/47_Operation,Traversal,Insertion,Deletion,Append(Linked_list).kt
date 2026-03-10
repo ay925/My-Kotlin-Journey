76922399dfc7f355dbb7ef9531c128bf6815f362
Anupam Yadav <anupamyadav4347@gmail.com>
2026-03-10 22:44:31 +0530

    .

--- src/DSA/Classes/47_Operation,Traversal,Insertion,Deletion,Append(Linked_list).kt
+++ src/DSA/Classes/47_Operation,Traversal,Insertion,Deletion,Append(Linked_list).kt
@@ -3,20 +3,6 @@ package DSA.Classes
 class Node(var value: Int) {
     var next: Node? = null
 }
-
-fun main() {
-
-    val node1 = Node(4)
-    val node2 = Node(10)
-    val node3 = Node(50)
-    val node4 = Node(20)
-
-    node1.next = node2
-    node2.next = node3
-    node3.next = node4
-
-    println(node1.next?.next?.next?.value ?: 0)
-
     class SinglyLinkedList {
 
         var head: Node? = null
@@ -36,5 +22,40 @@ fun main() {
                 curr?.next=newNode
             }
             }
+
+        fun traversal(){
+            if (head==null){
+                println("SLL is empty")
+            }else{
+                var curr=head
+                while (curr !=null){
+                    print("${curr.value} ")
+                    curr=curr.next
+                }
+            }
         }
+
     }
+
+fun main() {
+    /*
+    val node1 = Node(4)
+    val node2 = Node(10)
+    val node3 = Node(50)
+    val node4 = Node(20)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+
+    println(node1.next?.next?.next?.value ?: 0)
+    */
+
+    val sll= SinglyLinkedList()
+    sll.append(2)
+    sll.append(4)
+    sll.append(6)
+    sll.append(8)
+    sll.append(12)
+    sll.traversal()
+}

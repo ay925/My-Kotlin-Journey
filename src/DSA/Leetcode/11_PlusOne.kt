8993d2441b2a96c91226669f971c963904928d49
Anupam Yadav <ay846115@zohomail.in>
2026-04-04 23:53:23 +0530

    .

---
+++ src/DSA/Leetcode/11_PlusOne.kt
@@ -0,0 +1,23 @@
+package DSA.Leetcode
+
+import DSA.Classes.factorial
+
+class Solution11 {
+    fun plusOne(digits: IntArray): IntArray {
+        for (i in digits.size - 1 downTo 0) {
+            if (digits[i] < 9) {
+                digits[i]++
+                return digits
+            }
+            digits[i] = 0
+        }
+
+        val result = IntArray(digits.size + 1)
+        result[0] = 1
+        return result
+    }
+}
+fun main() {
+    val answer= Solution11()
+    print(answer.plusOne(intArrayOf(1,9,9,9)).joinToString())
+}

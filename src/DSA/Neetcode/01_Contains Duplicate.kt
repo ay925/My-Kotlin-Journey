d5a57941792018e90d794592d221ca7447b3977e
Anupam Yadav <ay846115@zohomail.in>
2026-04-06 23:03:09 +0530

    .

---
+++ src/DSA/Neetcode/01_Contains Duplicate.kt
@@ -0,0 +1,25 @@
+package DSA.Neetcode
+
+
+class Solution01 {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val frequencyOfNumbers=mutableMapOf<Int, Int>()
+        for (i in nums){
+            var count=1
+            if (i !in frequencyOfNumbers.keys){
+                frequencyOfNumbers[i]= 1
+            }else{
+                count++
+                frequencyOfNumbers[i]=count
+            }
+        }
+        for (i in frequencyOfNumbers.values){
+            if (i>1) return true
+        }
+        return false
+    }
+}
+fun main() {
+    val answer= Solution01()
+    print(answer.hasDuplicate(intArrayOf(1, 2, 3, 4)))
+}

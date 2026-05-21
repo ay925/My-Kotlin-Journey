6923732e06b723ad1bc38bad5f9c70544103e41a
Anupam Yadav <ay846115@zohomail.in>
2026-05-21 18:25:42 +0530

    .

--- src/self_learning/Result.kt
+++ src/self_learning/Result.kt
@@ -1,9 +1,13 @@
 package self_learning
 
-interface Result {
-    fun onSuccess()
-    fun onFailed(error: String)
+interface PermissionInternet{
+    fun accessPermission()
 }
+interface Result: PermissionInternet {
+    fun onSuccess()
+   fun onFailed(error: String)
+}
+
 class GetData: Result{
     override fun onSuccess() {
         println("Success to get data")
@@ -11,6 +15,10 @@ class GetData: Result{
 
     override fun onFailed(error: String) {
         println("Failed to get data")
+    }
+
+    override fun accessPermission() {
+        println("Get data access your permission")
     }
 
 }
@@ -23,9 +31,29 @@ class ClearData: Result{
         println("Data not cleaned please try again")
     }
 
+    override fun accessPermission() {
+        println("Get data access your permission")
+    }
+
+}
+class AccessUserPermission(): PermissionInternet, Result{
+    override fun accessPermission() {
+        println("User permission access")
+    }
+
+    override fun onSuccess() {
+        println("User permission granted")
+    }
+
+    override fun onFailed(error: String) {
+        println("User permission denied")
+    }
+
 }
 fun main() {
     val data= GetData()
+    val accessUserPermission= AccessUserPermission()
+    accessUserPermission.accessPermission()
     val clearData = ClearData()
     data.onFailed("dfd")
     clearData.onFailed("dfd")
